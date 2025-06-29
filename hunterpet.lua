ae164c584a5cd4f98f82506054bac72946c4df34
shagu <ericmauser@googlemail.com>
2025-06-29 14:43:59 +0200

    hunterpet: update to latest vmangos data

--- hunterpet.lua
+++ hunterpet.lua
@@ -620,7 +620,7 @@ end
 do -- database
   local creature_template = {}
   local query = mysql:execute([[
-    SELECT entry, patch, name, base_attack_time, rank, display_id1, pet_family, level_min, level_max, spell_list_id, pet_spell_list_id, unit_class FROM `creature_template` WHERE ( type_flags & 1) AND pet_family > 0 ORDER BY pet_family, level_min, level_max, name
+    SELECT entry, patch, name, base_attack_time, rank, display_id1, pet_family, level_min, level_max, spell_list_id, pet_spell_list_id, unit_class FROM `creature_template` WHERE ( static_flags1 & 16) AND pet_family > 0 ORDER BY pet_family, level_min, level_max, name
   ]])
 
   while query:fetch(creature_template, "a") do
@@ -654,7 +654,7 @@ do -- database
 
     pets[tableid] = pets[tableid] or {}
     pets[tableid].name = creature_template.name
-    pets[tableid].family = creature_template.beast_family
+    pets[tableid].family = creature_template.pet_family
     pets[tableid].level = level
     pets[tableid].id = entry
     pets[tableid].model = tonumber(creature_template.display_id1)
